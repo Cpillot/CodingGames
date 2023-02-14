6896b93c2f0dbf8ca929c9104a8915520f819b00
ChristophePillot <Christophe@open-it-solutions.com>
2023-02-14 14:51:57 +0100

    Init

---
+++ Marti.FizzBuzz/Program.cs
@@ -0,0 +1,40 @@
+﻿//From 1 to 100
+//If a number is divisible by 3, the print “fizz” rather than the number.
+//If a number is divisible by 5, print “buzz” rather than the number.
+//If a number is divisible by both, print “fizzbuzz”
+//Else print the number
+
+for (int i = 1; i <= 100; i++)
+{
+    string print = string.Empty;
+
+    if (i % 3 == 0)
+    {
+        print += "fizz";
+    }
+
+    if (i % 5 == 0)
+    {
+        print += "buzz";
+    }
+
+    if (string.IsNullOrEmpty(print))
+    {
+        print += i;
+    }
+
+    Console.WriteLine($"{print}");
+}
+
+// avec ternaire
+for (int i = 1; i <= 100; i++)
+{
+    string print = string.Empty;
+
+    print += i % 3 == 0 && i % 5 == 0 ? "fizzbuzz"
+            : i % 3 == 0 ? "fizz"
+            : i % 5 == 0 ? print += "buzz"
+            : i.ToString();
+
+    Console.WriteLine($"{print}");
+}
